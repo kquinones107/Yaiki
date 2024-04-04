09dbbdbc8e591633f88049100866f8f1d71ec0af
kquinones107 <kquinones@unal.edu.co>
2024-04-04 00:09:03 -0500

    instalando navigate

--- App.tsx
+++ App.tsx
@@ -1,29 +1,13 @@
-import { Image, View, StyleSheet, Dimensions } from 'react-native';
-const imagenFuente = require('./src/resources/assets/images/YaikiLogo.png');
+import React from 'react';
+import { View } from 'react-native';
+import LoginScreen from './src/screens'; // Ajusta la ruta según la ubicación real de tu archivo MiPantalla.tsx
 
-const LoginScreen = () => {
+function App() {
   return (
-      <View style={styles.container}>
-          <Image source={imagenFuente} style={styles.imagen} />
-      </View>
+    <View style={{ flex: 1 }}>
+      <LoginScreen />
+    </View>
   );
-};
+}
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
-
-const styles = StyleSheet.create({
-  container: {
-      backgroundColor: 'pink',
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-  },
-  imagen: {
-      width: 200, // ajusta el ancho según sea necesario
-      height: 200, // ajusta la altura según sea necesario
-  },
-});
-
-export default LoginScreen;
-
+export default App
