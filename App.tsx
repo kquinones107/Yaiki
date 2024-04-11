c246f080c1df9748434538300cd7cdcc36e1c2ca
Wilson Daniel Ospina <wdospinal@gmail.com>
2024-04-11 14:15:30 -0500

    funcionalidad: agregar navegacion de botones

--- App.tsx
+++ App.tsx
@@ -1,38 +1,54 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import LoginScreen from './src/screens/LoginScreen';
 import Welcome from './src/screens/Welcome';
 import Home from './src/screens/Home';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        options={{
+          headerShown: false,
+        }}
+        name="Home"
+        component={Home}
+      />
+    </Stack.Navigator>
+  );
+};
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="">
-        <Stack.Screen
+      <Tab.Navigator initialRouteName="">
+        <Tab.Screen
           options={{
             headerShown: false,
           }}
           name="Login"
           component={LoginScreen}
         />
-        <Stack.Screen
+        <Tab.Screen
           options={{
             headerShown: false,
           }}
           name="Welcome"
           component={Welcome}
         />
-        <Stack.Screen
+        <Tab.Screen
           options={{
             headerShown: false,
           }}
           name="Home"
-          component={Home}
+          component={HomeStack}
         />
-      </Stack.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
